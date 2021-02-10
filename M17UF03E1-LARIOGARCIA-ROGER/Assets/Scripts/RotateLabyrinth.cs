5ee11e22293f7c4f91fdd45db4dfee4e630e52c7
larios-gin <roger.lario.7e3@itb.cat>
2021-02-10 19:03:38 +0100

    First commit
    - Created the labyrinth level
    - Added a script that rotates the labyrinth

---
+++ M17UF03E1-LARIOGARCIA-ROGER/Assets/Scripts/RotateLabyrinth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLabyrinth : MonoBehaviour
+{
+    public Vector3 point;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.RotateAround(point, Vector3.up, 20 * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+            transform.RotateAround(point, -Vector3.up, 20 * Time.deltaTime);
+    }
+}
